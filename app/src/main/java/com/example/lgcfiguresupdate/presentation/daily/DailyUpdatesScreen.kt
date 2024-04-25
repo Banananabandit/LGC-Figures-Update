2010ad97fa9daba89577dbcd7d10cb92f0d5ff3e
Pavel <pavelcistjakov@gmail.com>
2024-04-25 13:02:07 +0100

    Adding input validator

--- app/src/main/java/com/example/lgcfiguresupdate/presentation/daily/DailyUpdatesScreen.kt
+++ app/src/main/java/com/example/lgcfiguresupdate/presentation/daily/DailyUpdatesScreen.kt
@@ -11,27 +11,36 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Refresh
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme.typography
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import java.math.RoundingMode
 import java.text.DecimalFormat
 import kotlin.math.roundToInt
 
 var salesExVAT = ""
+var updateMessage = ""
+
 @Composable
 fun DailyUpdatesScreen() {
     Column {
@@ -65,11 +74,35 @@ fun SubmitButton() {
 
 @Composable
 fun SalesUpdate() {
-    var text by remember { mutableStateOf("") }
-    Text(text = "Sales")
-    TextField(
+    var text by rememberSaveable { mutableStateOf("") }
+    var isError by rememberSaveable { mutableStateOf(false) }
+    val errorMessage = "Too many numbers!"
+    val charLimit = 6
+
+    fun validate(text: String) {
+        isError = text.length > charLimit
+    }
+
+    OutlinedTextField(
         value = text,
-        onValueChange = {text = it})
+        onValueChange = {
+            text = it
+            validate(text) },
+        singleLine = true,
+        label = { Text(if (isError) "Sales*" else "Sales")},
+        supportingText = {
+                         Text(
+                             modifier = Modifier.fillMaxWidth(),
+                             text = "Limit: ${text.length}/$charLimit",
+                             textAlign = TextAlign.End)
+        },
+        isError = isError,
+        keyboardActions = KeyboardActions{validate(text)},
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+        modifier = Modifier.semantics {
+            if (isError) error(errorMessage)
+        }.fillMaxWidth()
+    )
     Row {
         Text(text = "Result")
         Result(calculateExVAT(text))
@@ -80,7 +113,7 @@ fun SalesUpdate() {
 fun CustomerNumberUpdate() {
     var text by remember { mutableStateOf("") }
     Text(text = "Number of Customers")
-    TextField(
+    OutlinedTextField(
         value = text,
         onValueChange = {text = it})
     Row {
@@ -151,8 +184,6 @@ fun Result(result: String) {
 }
 
 fun calculateExVAT(result: String): String {
-    // Write something more elegant in here
-    // Also add logic to prevent letters to be passed through
     var result1 = result
     if (result.isNotEmpty()){
         salesExVAT = (result.toDouble() / 1.2).roundToInt().toString()
@@ -170,18 +201,15 @@ fun calculateATV(text: String) : String {
     return text
 }
 
-//Figure out what the fuck happened here
 private fun whatsAppResultShare(context: Context) {
-    Intent(Intent.ACTION_SEND).also {// launch main activity of the target app
+    Intent(Intent.ACTION_SEND).also {
         it.setPackage("com.whatsapp")
         it.putExtra(Intent.EXTRA_TEXT, "THIS IS A TEST")
         it.type = "text/plain"
         try {
             context.startActivity(it)
-//            startActivity(context, it, )
         } catch (e: ActivityNotFoundException) {
             e.printStackTrace()
         }
     }
-
 }
